1a75897d5eef74da913767ad9c66770248cdaa41
Joshua S. Muse <sullivan.muse@gmail.com>
2025-04-22 09:11:56 -0500

    implement

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,23 @@
 use std::collections::HashMap;
 
+struct Flags {
+    polymorphic_let: bool,
+}
+
+impl Flags {
+    fn all() -> Self {
+        Self {
+            polymorphic_let: true,
+        }
+    }
+
+    fn none() -> Self {
+        Self {
+            polymorphic_let: false,
+        }
+    }
+}
+
 // types
 #[derive(Clone, Debug, PartialEq, Eq)]
 enum Error {
@@ -12,10 +30,10 @@ type Id<'id> = &'id str;
 
 #[derive(Clone, Debug, PartialEq, Eq)]
 enum Expr<'id> {
-    Id(Id<'id>),
-    Fn(Id<'id>, Box<Self>),
-    Let(Id<'id>, Box<Self>, Box<Self>),
-    Call(Box<Self>, Box<Self>),
+    Id(Id<'id>),                                      // x
+    Fn(Id<'id>, Box<Self>),                           // x -> x
+    Let(Id<'id>, Vec<Id<'id>>, Box<Self>, Box<Self>), // let f x y = v; b
+    Call(Box<Self>, Box<Self>),                       // f x
 }
 
 type Var = usize;
@@ -26,37 +44,60 @@ enum Type {
     Fn(Box<Self>, Box<Self>),
 }
 
+#[derive(Clone, Debug, PartialEq, Eq)]
+struct Scheme {
+    bounds: Vec<Var>,
+    ty: Type,
+}
+
 type Unifier = HashMap<Var, Type>;
 
 #[derive(Clone, Debug, PartialEq, Eq)]
 struct Context<'id> {
     vars: usize,
-    types: Vec<(Id<'id>, Type)>,
+    scopes: Vec<HashMap<Id<'id>, Scheme>>,
 }
 
 // functions
 impl<'id> Expr<'id> {
-    fn infer(&self, context: &mut Context<'id>) -> Result<Type, Error> {
+    fn infer(&self, context: &mut Context<'id>, flags: &Flags) -> Result<Type, Error> {
         match self {
             Self::Id(id) => context.get(id).map(|ty| ty.clone()),
-            Self::Let(k, v, body) => {
-                let ty = v.infer(context)?;
-                context.insert(k, ty);
-                let result = body.infer(context);
-                context.pop();
+            Self::Let(key, params, value, body) => {
+                context.enter();
+                let scheme = {
+                    let prev_vars = context.vars;
+                    context.enter();
+                    let mut bounds = Vec::new();
+                    for p in params {
+                        let var = context.fresh();
+                        bounds.push(var.clone());
+                        let scheme = Scheme::from(Type::Var(var));
+                        context.insert(p, scheme);
+                    }
+                    let ty = value.infer(context, flags)?;
+                    context.exit();
+                    let out = Scheme { bounds, ty };
+                    context.vars = prev_vars;
+                    out
+                };
+                context.insert(key, scheme);
+                let result = body.infer(context, flags);
+                context.exit();
                 result
             }
             Self::Fn(x, body) => {
+                context.enter();
                 let k = context.fresh();
-                context.insert(x, k.clone());
-                let ty = body.infer(context)?;
-                context.pop();
-                Ok(Type::Fn(Box::new(k), Box::new(ty)))
+                context.insert(x, Scheme::from(Type::Var(k)));
+                let ty = body.infer(context, flags)?;
+                context.exit();
+                Ok(Type::Fn(Box::new(Type::Var(k)), Box::new(ty)))
             }
             Self::Call(f, x) => {
-                let f_ty = f.infer(context)?;
+                let f_ty = f.infer(context, flags)?;
                 if let Type::Fn(param_ty, mut body_ty) = f_ty {
-                    let x_ty = x.infer(context)?;
+                    let x_ty = x.infer(context, flags)?;
                     let mut unifier = HashMap::new();
                     param_ty.unify(&x_ty, &mut unifier)?;
                     body_ty.subst(&unifier);
@@ -105,35 +146,74 @@ impl Type {
     }
 }
 
+impl Scheme {
+    fn from(ty: Type) -> Self {
+        Self {
+            bounds: Vec::new(),
+            ty,
+        }
+    }
+}
+
 impl<'id> Context<'id> {
     fn new() -> Self {
         Self {
             vars: 0,
-            types: Vec::new(),
+            scopes: Vec::new(),
         }
     }
 
-    fn fresh(&mut self) -> Type {
+    fn last(&self) -> &HashMap<Id<'id>, Scheme> {
+        self.scopes.last().expect("never entered a scope")
+    }
+
+    fn last_mut(&mut self) -> &mut HashMap<Id<'id>, Scheme> {
+        self.scopes.last_mut().expect("never entered a scope")
+    }
+
+    fn enter(&mut self) {
+        self.scopes.push(HashMap::new());
+    }
+
+    fn exit(&mut self) {
+        self.scopes.pop().expect("never entered a scope");
+    }
+
+    fn fresh(&mut self) -> Var {
         let out = self.vars;
         self.vars += 1;
-        Type::Var(out)
+        out
     }
 
-    fn get(&self, id: Id<'id>) -> Result<&Type, Error> {
-        for (k, v) in self.types.iter().rev() {
-            if *k == id {
-                return Ok(v);
+    fn instantiate(&mut self, scheme: &Scheme) -> Type {
+        let mut unifier = HashMap::new();
+        for b in &scheme.bounds {
+            let f = self.fresh();
+            unifier.insert(*b, Type::Var(f));
+        }
+        let mut ty = scheme.ty.clone();
+        ty.subst(&unifier);
+        ty
+    }
+
+    fn get(&mut self, id: Id<'id>) -> Result<Type, Error> {
+        let mut result: Option<Scheme> = None;
+        for scope in self.scopes.iter().rev() {
+            if let Some(scheme) = scope.get(id) {
+                result = Some(scheme.clone());
+                break;
             }
         }
+        if let Some(scheme) = result {
+            let ty = self.instantiate(&scheme);
+            return Ok(ty);
+        }
+
         Err(Error::Undefined)
     }
 
-    fn pop(&mut self) {
-        self.types.pop().expect("pop must follow insert");
-    }
-
-    fn insert(&mut self, id: Id<'id>, ty: Type) {
-        self.types.push((id, ty));
+    fn insert(&mut self, id: Id<'id>, scheme: Scheme) {
+        self.last_mut().insert(id, scheme);
     }
 }
 
@@ -145,7 +225,8 @@ mod test {
     fn test_undefined1() {
         let mut context = Context::new();
         let e = Expr::Id("xyz");
-        let result = e.infer(&mut context);
+        let flags = Flags::all();
+        let result = e.infer(&mut context, &flags);
         assert_eq!(
             result,
             Err(Error::Undefined),
@@ -161,6 +242,7 @@ mod test {
             "y",
             Box::new(Expr::Let(
                 "id",
+                Vec::new(),
                 Box::new(Expr::Fn("x", Box::new(Expr::Id("x")))),
                 Box::new(Expr::Call(
                     Box::new(Expr::Id("id")),
@@ -168,7 +250,8 @@ mod test {
                 )),
             )),
         );
-        let result = e.infer(&mut context);
+        let flags = Flags::all();
+        let result = e.infer(&mut context, &flags);
         assert_eq!(
             result,
             Err(Error::Undefined),
@@ -180,7 +263,8 @@ mod test {
     fn infer_ident_fn() {
         let mut context = Context::new();
         let e = Expr::Fn("x", Box::new(Expr::Id("x")));
-        let result = e.infer(&mut context);
+        let flags = Flags::all();
+        let result = e.infer(&mut context, &flags);
         assert_eq!(
             result,
             Ok(Type::Fn(Box::new(Type::Var(0)), Box::new(Type::Var(0)))),
@@ -192,8 +276,9 @@ mod test {
     fn test_let() {
         let mut context = Context::new();
         let id = Expr::Fn("x", Box::new(Expr::Id("x")));
-        let e = Expr::Let("id", Box::new(id), Box::new(Expr::Id("id")));
-        let result = e.infer(&mut context);
+        let e = Expr::Let("id", Vec::new(), Box::new(id), Box::new(Expr::Id("id")));
+        let flags = Flags::all();
+        let result = e.infer(&mut context, &flags);
         assert_eq!(
             result,
             Ok(Type::Fn(Box::new(Type::Var(0)), Box::new(Type::Var(0)))),
@@ -209,6 +294,7 @@ mod test {
             "y",
             Box::new(Expr::Let(
                 "id",
+                Vec::new(),
                 Box::new(Expr::Fn("x", Box::new(Expr::Id("x")))),
                 Box::new(Expr::Call(
                     Box::new(Expr::Id("id")),
@@ -216,7 +302,8 @@ mod test {
                 )),
             )),
         );
-        let result = e.infer(&mut context);
+        let flags = Flags::all();
+        let result = e.infer(&mut context, &flags);
         assert_eq!(
             result,
             Ok(Type::Fn(
@@ -235,6 +322,7 @@ mod test {
             "y",
             Box::new(Expr::Let(
                 "id",
+                Vec::new(),
                 Box::new(Expr::Fn("x", Box::new(Expr::Id("x")))),
                 Box::new(Expr::Call(
                     Box::new(Expr::Id("id")),
@@ -242,11 +330,54 @@ mod test {
                 )),
             )),
         );
-        let result = e.infer(&mut context);
+        let flags = Flags::all();
+        let result = e.infer(&mut context, &flags);
         assert_eq!(
             result,
             Ok(Type::Fn(Box::new(Type::Var(0)), Box::new(Type::Var(0)))),
             "ident of var has var type"
+        )
+    }
+
+    #[test]
+    fn test_polymorphic_id() {
+        // let id = x -> x,
+        //     first = x y -> x;
+        // first (id id) (id first)
+        //     => a -> a
+        let mut context = Context::new();
+        let expr = Expr::Let(
+            "id",
+            vec![],
+            Box::new(Expr::Fn("x", Box::new(Expr::Id("x")))),
+            Box::new(Expr::Let(
+                "first",
+                vec![],
+                Box::new(Expr::Fn(
+                    "x",
+                    Box::new(Expr::Fn("y", Box::new(Expr::Id("x")))),
+                )),
+                Box::new(Expr::Call(
+                    Box::new(Expr::Call(
+                        Box::new(Expr::Id("first")),
+                        Box::new(Expr::Call(
+                            Box::new(Expr::Id("id")),
+                            Box::new(Expr::Id("id")),
+                        )),
+                    )),
+                    Box::new(Expr::Call(
+                        Box::new(Expr::Id("id")),
+                        Box::new(Expr::Id("first")),
+                    )),
+                )),
+            )),
+        );
+        let flags = Flags::all();
+        let result = expr.infer(&mut context, &flags);
+        assert_eq!(
+            result,
+            Ok(Type::Fn(Box::new(Type::Var(0)), Box::new(Type::Var(0)))),
+            "type checker supports polymorphic identifier function"
         )
     }
 }
@@ -254,10 +385,11 @@ mod test {
 fn main() {
     // y -> let id = x -> x; id x
     let mut context = Context::new();
-    let e = Expr::Fn(
+    let expr = Expr::Fn(
         "y",
         Box::new(Expr::Let(
             "id",
+            Vec::new(),
             Box::new(Expr::Fn("x", Box::new(Expr::Id("x")))),
             Box::new(Expr::Call(
                 Box::new(Expr::Id("id")),
@@ -265,6 +397,7 @@ fn main() {
             )),
         )),
     );
-    let result = e.infer(&mut context);
+    let flags = Flags::all();
+    let result = expr.infer(&mut context, &flags);
     let _ = dbg!(result);
 }
